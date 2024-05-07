f27f7e53dcdada10eacaeea99dbff27078f16cae
ClauuGithub <164614753+ClauuGithub@users.noreply.github.com>
2024-05-07 18:05:09 +0200

    He añadido botones al merch

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*desplegable faq*/
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,3 +13,10 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+/*botones de patrocinar*/
+document.querySelectorAll(".boton").forEach(boton => {
+  boton.addEventListener("click", function() {
+    alert("¡Gracias!");
+  });
+});
