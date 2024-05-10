55bbea544ac4e0a1c3771f94fbf1aa0ce36f9875
Anon2001G <gabricanito13@gmail.com>
2024-05-10 22:36:56 +0200

    He añadido la animación del scroll con una transición y un cambio de opacidad. También he puesto sombra a los títulos. :D

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /*desplegable faq*/
 var acc = document.getElementsByClassName("accordion");
+var campaña = document.querySelectorAll(".hidden");
+
 var i;
 
 for (i = 0; i < acc.length; i++) {
@@ -20,3 +22,16 @@ document.querySelectorAll(".boton").forEach(boton => {
     alert("¡Gracias!");
   });
 });
+
+
+const observer = new IntersectionObserver((e) => {
+  e.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("hidden");
+      entry.target.classList.add("show");
+    }
+  })
+})
+
+campaña.forEach((el) => observer.observe(el));
